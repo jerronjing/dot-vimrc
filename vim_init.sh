a93036de26ca7f574b79fb1ded67d497363e0cea
景罗 <jerronjing@sohu-inc.com>
2015-09-29 09:46:42 +0800

    修改初始化脚本

--- vim_init.sh
+++ vim_init.sh
@@ -1,11 +1,11 @@
-!#/bin/sh
+#!/bin/bash
 
 #1、back up origin vim config file and directory
 mv ~/.vimrc ~/.vimrc.org
 mv ~/.vim ~/.vim.org
 
 #2、git sysc file and directory
-git clone git://github.com/jerronking/dot-vimrc.git ~/.vim
+git clone git@github.com:jerronjing/dot-vimrc.git ~/.vim
 ln -s ~/.vim/vimrc ~/.vimrc
 
 #3、clone vundle project
